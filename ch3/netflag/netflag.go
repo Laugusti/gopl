c9be4f92f75d053c32e0bf9e2160302cda401ee7
Leon Augustine <laugusti@mail.usf.edu>
2017-01-22 14:10:18 -0500

    ch 3 examples/exercises

---
+++ ch3/netflag/netflag.go
@@ -0,0 +1,23 @@
+package main
+
+type Flags uint
+
+const (
+	FlagUp           Flags = 1 << iota // is up
+	FlagBroadcast                      // supports broadcast access capability
+	FlagLoopback                       // is a loopback interface
+	FlagPointToPoint                   // belogns to a point-tpoint link
+	FlagMulticast                      // supports multicast access capability
+)
+
+func IsUp(v Flags) bool     { return v&FlagUp == FlagUp }
+func TurnDown(v *Flags)     { *v &^= FlagUp }
+func SetBroadcast(v *Flags) { *v |= FlagBroadcast }
+func IsCast(v Flags) bool   { return v^(FlagBroadcast|FlagMulticast) != 0 }
+
+func main() {
+	var v Flags = FlagMulticast | FlagUp
+	fmt.Printf("%b %t\n", v, isUp(v)) // "10001 true"
+	TurnDown(&v)
+	fmt.
+}
