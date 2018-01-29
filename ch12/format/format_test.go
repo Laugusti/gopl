f31411260cd2a5af8c0ba5ec412a24bddebbd7f6
Leon <laugusti@mail.usf.edu>
2018-01-29 00:19:30 -0500

    ch 12 examples/exercises

--- ch12/format/format_test.go
+++ ch12/format/format_test.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 	"testing"
 	"time"
+
+	"gopl.io/ch12/format"
 )
 
 func TestAny(t *testing.T) {
-	fmt.Println(Any(2 * time.Nanosecond))
-	fmt.Println(Any(time.Now))
-	fmt.Println(Any(true))
+	var x int64 = 1
+	var d time.Duration = 1 * time.Nanosecond
+	fmt.Println(format.Any(x))                  // "1"
+	fmt.Println(format.Any(d))                  // "1"
+	fmt.Println(format.Any([]int64{x}))         // "[]int64 0xc420010170"
+	fmt.Println(format.Any([]time.Duration{d})) // "[]time.Duration 0xc420010178"
 }
