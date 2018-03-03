64afd033fbaa90d458af9a858b5cdf863f16314e
Leon <laugusti@mail.usf.edu>
2018-03-03 17:13:57 -0500

    ch 12 examples/exercises

---
+++ ch12/display/cmd/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "github.com/Laugusti/gopl/ch12/display"
+
+type Movie struct {
+	Title, Subtitle string
+	Year            int
+	Color           bool
+	Actor           map[string]string
+	Oscars          []string
+	Sequel          *string
+}
+
+func main() {
+	strangelove := Movie{
+		Title:    "Dr. Strangelove",
+		Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
+		Year:     1964,
+		Color:    true,
+		Actor: map[string]string{
+			"Dr. Srangelove":             "Peter Sellers",
+			"Grp. Capt. Lionel Mandrake": "Peter Sellers",
+			"Pres. Merkin Muffley":       "Peter Sellers",
+			"Gen. Buck Turgidson":        "George C. Scott",
+			"Brig. Gen. Jack D. Ripper":  "Sterling Hayden",
+			`Maj. T.J. "King" Kong`:      "Slim Pickens",
+		},
+		Oscars: []string{
+			"Best Actor (Nomin.)",
+			"Best Adapted Screenplay (Nomin.)",
+			"Best Director (Nomin.)",
+			"Best Picture (Nomin.)",
+		},
+	}
+
+	display.Display("strangelove", strangelove)
+}
