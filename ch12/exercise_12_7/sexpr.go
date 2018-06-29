578795328d737ff18bac68b66d03e61f8487dfa7
Leon <laugusti@mail.usf.edu>
2018-06-29 19:04:21 -0400

    ch 12 examples/exercises

--- ch12/exercise_12_7/sexpr.go
+++ ch12/exercise_12_7/sexpr.go
@@ -8,6 +8,18 @@ import (
 	"strings"
 )
 
+type Decoder struct {
+	r io.Reader
+}
+
+func NewDecoder(r io.Reader) *Decoder {
+	return &Decoder{r}
+}
+
+func (dec *Decoder) Decode(v interface{}) error {
+	return nil
+}
+
 type Encoder struct {
 	w io.Writer
 }
