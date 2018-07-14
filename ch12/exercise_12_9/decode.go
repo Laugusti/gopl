335e0f8cb45acd4f1faf957b1d409adedf4cdddd
Leon <laugusti@mail.usf.edu>
2018-07-14 04:32:47 -0400

    ch 12 examples/exercises

--- ch12/exercise_12_9/decode.go
+++ ch12/exercise_12_9/decode.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io"
 	"reflect"
-	"text/scanner"
 )
 
 // A Decoder reads an decodes a S-Expression from an input stream.
@@ -133,19 +132,4 @@ func getToken(dec *Decoder) (Token, bool) {
 	}
 	_, isEnd := tok.(EndList)
 	return tok, isEnd
-}
-
-type lexer struct {
-	scan  scanner.Scanner
-	token rune // the current token
-}
-
-func (lex *lexer) next()        { lex.token = lex.scan.Scan() }
-func (lex *lexer) text() string { return lex.scan.TokenText() }
-
-func (lex *lexer) consume(want rune) {
-	if lex.token != want { // NOTE: Not an example of good error handling.
-		panic(fmt.Sprintf("got %q, want %q", lex.text(), want))
-	}
-	lex.next()
 }
