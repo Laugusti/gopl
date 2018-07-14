eaeef34007dc9aaa1f7b30da2fcfc2869a342fe0
Leon <laugusti@mail.usf.edu>
2018-07-14 04:28:58 -0400

    ch 12 examples/exercises

--- ch12/exercise_12_9/decode.go
+++ ch12/exercise_12_9/decode.go
@@ -27,8 +27,112 @@ func (dec *Decoder) Decode(v interface{}) (err error) {
 			err = fmt.Errorf("error at %s: %v", dec.lex.scan.Position, x)
 		}
 	}()
-	setValueFromTokens(reflect.ValueOf(v).Elem(), dec.getAllTokens())
+	for {
+		tok, err := dec.Token()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+		setValueFromToken(dec, tok, reflect.ValueOf(v).Elem())
+	}
 	return nil
+}
+
+func setValueFromToken(dec *Decoder, tok Token, v reflect.Value) {
+	switch tok := tok.(type) {
+	case Symbol:
+		if tok.Name == "nil" {
+			v.Set(reflect.Zero(v.Type()))
+		}
+	case Int:
+		v.SetInt(int64(tok.Value))
+	case String:
+		v.SetString(tok.Text)
+	case StartList:
+		decodeList(dec, v)
+	default:
+		panic(fmt.Sprintf("unexpected token %T", tok))
+	}
+}
+
+func decodeList(dec *Decoder, v reflect.Value) {
+	switch v.Kind() {
+	case reflect.Array: // (item ...)
+		for i := 0; ; i++ {
+			tok, endOfList := getToken(dec)
+			if endOfList {
+				break
+			}
+			setValueFromToken(dec, tok, v.Index(i))
+		}
+	case reflect.Slice: // (item ...)
+		for i := 0; ; i++ {
+			tok, endOfList := getToken(dec)
+			if endOfList {
+				break
+			}
+			item := reflect.New(v.Type().Elem()).Elem()
+			setValueFromToken(dec, tok, item)
+			v.Set(reflect.Append(v, item))
+		}
+	case reflect.Struct: // ((name value) ...)
+		for i := 0; ; i++ {
+			tok, endOfList := getToken(dec)
+			if endOfList {
+				break
+			}
+			if _, isStartList := tok.(StartList); !isStartList {
+				panic(fmt.Sprintf("wanted StartList, got %T", tok))
+			}
+			tok, _ = getToken(dec)
+			if _, isSymbol := tok.(Symbol); !isSymbol {
+				panic(fmt.Sprintf("wanted Symbol, got %T", tok))
+			}
+			name := tok.(Symbol).Name
+			tok, _ = getToken(dec)
+			setValueFromToken(dec, tok, v.FieldByName(name))
+			tok, _ = getToken(dec)
+			if _, isEnd := tok.(EndList); !isEnd {
+				panic(fmt.Sprintf("wanted EndList, got %T", tok))
+			}
+		}
+	case reflect.Map: // ((key value) ...)
+		v.Set(reflect.MakeMap(v.Type()))
+		for i := 0; ; i++ {
+			tok, endOfList := getToken(dec)
+			if endOfList {
+				break
+			}
+			if _, isStartList := tok.(StartList); !isStartList {
+				panic(fmt.Sprintf("wanted StartList, got %T", tok))
+			}
+			key := reflect.New(v.Type().Key()).Elem()
+			tok, _ = getToken(dec)
+			setValueFromToken(dec, tok, key)
+			value := reflect.New(v.Type().Elem()).Elem()
+			tok, _ = getToken(dec)
+			setValueFromToken(dec, tok, value)
+			v.SetMapIndex(key, value)
+			tok, _ = getToken(dec)
+			if _, isEnd := tok.(EndList); !isEnd {
+				panic(fmt.Sprintf("wanted EndList, got %T", tok))
+			}
+		}
+	default:
+		panic(fmt.Sprintf("cannot decode list into %v", v.Type()))
+	}
+}
+func getToken(dec *Decoder) (Token, bool) {
+	tok, err := dec.Token()
+	if err == io.EOF {
+		panic("end of file")
+	}
+	if err != nil {
+		panic(err)
+	}
+	_, isEnd := tok.(EndList)
+	return tok, isEnd
 }
 
 type lexer struct {
@@ -44,107 +148,4 @@ func (lex *lexer) consume(want rune) {
 		panic(fmt.Sprintf("got %q, want %q", lex.text(), want))
 	}
 	lex.next()
-}
-
-func (dec *Decoder) getAllTokens() []Token {
-	var tokens []Token
-	for {
-		tok, err := dec.Token()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			panic(err)
-		}
-		tokens = append(tokens, tok)
-	}
-	return tokens
-}
-
-func setValueFromTokens(v reflect.Value, tokens []Token) {
-	switch {
-	case len(tokens) == 1:
-		setValue(v, tokens[0])
-	case len(tokens) > 1:
-		setListValue(v, tokens)
-	default:
-		panic("no tokens read")
-	}
-}
-
-func setValue(v reflect.Value, token Token) {
-	switch token := token.(type) {
-	case Symbol:
-		if token.Name == "nil" {
-			v.Set(reflect.Zero(v.Type()))
-		}
-	case Int:
-		v.SetInt(int64(token.Value))
-	case String:
-		v.SetString(token.Text)
-	default:
-		panic(fmt.Sprintf("unexpected token %T", token))
-	}
-}
-
-func setListValue(v reflect.Value, tokens []Token) {
-	switch v.Kind() {
-	case reflect.Array: // (item ...)
-		for i, indexTokens := range getListAsTokenList(tokens) {
-			setValueFromTokens(v.Index(i), indexTokens)
-		}
-	case reflect.Slice: // (item ...)
-		for _, indexTokens := range getListAsTokenList(tokens) {
-			item := reflect.New(v.Type().Elem()).Elem()
-			setValueFromTokens(item, indexTokens)
-			v.Set(reflect.Append(v, item))
-		}
-	case reflect.Struct: // ((name value) ...)
-		for _, indexTokens := range getListAsTokenList(tokens) {
-			nameValuePair := getListAsTokenList(indexTokens)
-			if _, isSymbol := nameValuePair[0][0].(Symbol); !isSymbol {
-				panic(fmt.Sprintf("got token %T, want field Symbol", nameValuePair[0]))
-			}
-			name := nameValuePair[0][0].(Symbol).Name
-			setValueFromTokens(v.FieldByName(name), nameValuePair[1])
-		}
-	case reflect.Map: // ((key value) ...)
-		v.Set(reflect.MakeMap(v.Type()))
-		for _, indexTokens := range getListAsTokenList(tokens) {
-			nameValuePair := getListAsTokenList(indexTokens)
-			key := reflect.New(v.Type().Key()).Elem()
-			setValueFromTokens(key, nameValuePair[0])
-			value := reflect.New(v.Type().Elem()).Elem()
-			setValueFromTokens(value, nameValuePair[1])
-			v.SetMapIndex(key, value)
-		}
-	default:
-		panic(fmt.Sprintf("cannot decode list into %v", v.Type()))
-	}
-}
-
-func getListAsTokenList(tokens []Token) [][]Token {
-	if _, start := tokens[0].(StartList); !start {
-		panic(fmt.Sprintf("unexpected token %T", tokens[0]))
-	}
-	if _, end := tokens[len(tokens)-1].(EndList); !end {
-		panic("end of file")
-	}
-	tokens = tokens[1 : len(tokens)-1]
-	var tokensList [][]Token
-	count := 0
-	curElementStartIndex := 0
-	for i, tok := range tokens {
-		switch tok.(type) {
-		case StartList:
-			count++
-		case EndList:
-			count--
-		}
-		if count <= 0 {
-			tokensList = append(tokensList, tokens[curElementStartIndex:i+1])
-			curElementStartIndex = i + 1
-		}
-	}
-	return tokensList
 }
