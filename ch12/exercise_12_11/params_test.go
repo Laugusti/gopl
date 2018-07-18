a369933978cee61d1de1117fa400abc532b25c36
Leon <laugusti@mail.usf.edu>
2018-07-17 20:13:54 -0400

    ch 12 examples/exercises

---
+++ ch12/exercise_12_11/params_test.go
@@ -0,0 +1,31 @@
+package params
+
+import "testing"
+
+func TestPack(t *testing.T) {
+	tests := []struct {
+		rawURL string
+		input  interface{}
+		want   string
+	}{
+		{"", struct{ a int }{3}, "?a=3"},
+		{"http://example.com", struct {
+			a string `http:"b"`
+		}{"testing"}, "http://example.com?b=testing"},
+		{"https://example.com?l=English#fragment", struct {
+			x        bool
+			language string `http:"l"`
+			maxSize  int    `http:"max"`
+		}{language: "官话", maxSize: 100}, "https://example.com?l=English&l=%E5%AE%98%E8%AF%9D&max=100&x=false#fragment"},
+	}
+
+	for _, test := range tests {
+		got, err := Pack(test.rawURL, test.input)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if got != test.want {
+			t.Errorf("TestPack => input: %v, expected: %v, got: %v", test.input, test.want, got)
+		}
+	}
+}
