32476a3e065bdf2102211a08d4e89224926c210e
Leon <laugusti@mail.usf.edu>
2018-07-23 01:39:40 -0400

    ch 13 examples/exercises

---
+++ ch13/bzipper/main.go
@@ -0,0 +1,20 @@
+// Bzipper reads input, bzip2-compresses it, and writes it out.
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+
+	"github.com/Laugusti/gopl/ch13/bzip"
+)
+
+func main() {
+	w := bzip.NewWriter(os.Stdout)
+	if _, err := io.Copy(w, os.Stdin); err != nil {
+		log.Fatalf("bzipper: %v\n", err)
+	}
+	if err := w.Close(); err != nil {
+		log.Fatalf("bzipper: close: %v\n", err)
+	}
+}
